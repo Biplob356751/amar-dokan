b1900871f4c048deb08ec532630716260ddce0a8
biplob <biplobhossain356751@gmail.com>
2021-09-03 11:06:30 -0600

    added

--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -5,36 +5,61 @@ import { useParams } from 'react-router-dom';
 
 const Product = () => {
     const { key } = useParams();
-    console.log(key);
-    const [products, setProducts] = useState([]);
+
+    
+    // console.log(key);
+    const [products, setProducts] = useState({});
+    const [result, setResult] = useState([]);
+
+
+    
+    
 
     useEffect(() => {
 
-        for (let i = 0; i < ShopCatagoryData.length; i++) {
-            if(ShopCatagoryData[i].catagory[i].key === key){
-                setProducts(ShopCatagoryData[i].catagory[i].product);
-            }
-            console.log(ShopCatagoryData[i].catagory[i].product);
-        }
-    }, [key,products]);
+        // for (let i = 0; i < ShopCatagoryData.length; i++) {
+        //     if(ShopCatagoryData[i].catagory[i].key == key){
+        //         setProducts(ShopCatagoryData[i].catagory[i].product);
+        //     }
+        //     // console.log(ShopCatagoryData[i].catagory[i].product);
+        //     console.log(products.id)
+        // }
 
+        setResult(ShopCatagoryData);
+    }, []);
+
+
+   const pd = result.map(res => {
+        const data = res.catagory.find(cat => cat.key == key);
+        // console.log(data);
+       setProducts(data);
+        return data;
+        
+
+    })
+     console.log(products)
+    
+    // 
+// console.log(product[0])
 
 
 
     return (
         <div className="product_page container">
             
-            {
-                products.map(productItem =>
-                    <div className="product_content" key={productItem.id}>
-                        <h1>{productItem.storId}</h1>
-                        <h1>{productItem.description}</h1>
-                        <img src={productItem.image} alt="" />
-                        <h1>{productItem.productName}</h1>
-                        <p>{productItem.discount}</p>
+            {/* {
+             pd?.product.map(productItem =>
+                    <div className="product_content" key={productItem ?.id}>
+                        
+                        <h1>{productItem ?.description}</h1>
+                        <img src={productItem ?.image} alt="" />
+                        <h1>{productItem ?.productName}</h1>
+                        <p>{productItem ?.discount}</p>
                     </div>
                 )
-            }
+            } */}
+
+            
         </div>
     );
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -6,7 +6,9 @@ import ShopCatagoryData from '../ShopCatagory/ShopCatagoryData/ShopCatagoryData.
 const Shop = (props) => {
     const { id } = useParams();
     const [shopItem, setShopItem] = useState([]);
+
     useEffect(() => {
+
         for (let i = 0; i < ShopCatagoryData.length; i++) {
             if (ShopCatagoryData[i].id == id) {
                 setShopItem(ShopCatagoryData[i].catagory);
@@ -14,14 +16,17 @@ const Shop = (props) => {
         }
 
     }, [id]);
+
+
     return (
         <div className="shop_container container">
             {
-                shopItem.map(shop =>
+               shopItem.map(shop =>
                     <div className="shop_content_img" key={shop.key}>
                         <Link to={`/product/${shop.key}`}>
                             <img src={shop.img} alt="" />
                             <div className="shop_content">
+                                <h1>{shop.key}</h1>
                                 <h4>{shop.name}</h4>
                                 <p>{shop.schedule}</p>
                             </div>
